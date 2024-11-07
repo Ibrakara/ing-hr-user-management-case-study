e1c221239cfd13ed18c69a3ec8c150bbc32f531c
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2024-11-07 18:33:27 +0300

    create pagination

---
+++ dev/components/listing-pagination.js
@@ -0,0 +1,89 @@
+import {LitElement, html, css} from 'lit';
+import {ICON} from '../constants';
+
+export class ListingPagination extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        div {
+          display: flex;
+          margin: 20px 0 20px 0;
+          justify-content: center;
+          align-items: center;
+          justify-items: center;
+        }
+        p {
+          display: inline;
+          list-style: none;
+          text-align: center;
+          width: 1.5rem;
+          margin: 0 0.2rem 0 0;
+          border-radius: 1.5rem;
+          background-color: transparent;
+          cursor: pointer;
+          font-size: 12px;
+          font-weight: 400;
+          color: #555665;
+          user-select: none;
+        }
+        .active {
+          background-color: #ff6200;
+          color: #fff;
+          text-align: center;
+        }
+      }
+    `;
+  }
+
+  static properties = {
+    count: {type: Number},
+    currentPage: {type: Number},
+  };
+
+  constructor() {
+    super();
+    this.count = 0;
+    this.currentPage = 1;
+  }
+
+  render() {
+    return html`
+      <div>
+        <custom-button
+          @click=${() => this.setCurrentPageNumber(this.currentPage - 1 || 1)}
+          .icon=${ICON.LEFT_ARROW}
+        ></custom-button>
+        ${this._pageList(this.count).map(
+          (page, index) =>
+            html`<p
+              @click=${() => this.setCurrentPageNumber(index + 1)}
+              class="${this.currentPage === index + 1 ? 'active' : ''}"
+            >
+              ${page}
+            </p>`
+        )}
+        <custom-button
+          @click=${() =>
+            this.setCurrentPageNumber(
+              this.currentPage + 1 > this.count
+                ? this.count
+                : this.currentPage + 1
+            )}
+          .icon=${ICON.RIGHT_ARROW}
+        ></custom-button>
+      </div>
+    `;
+  }
+  _pageList(count) {
+    const pageArray = Array.from({length: count}, (_, i) => i + 1);
+    return pageArray;
+  }
+  setCurrentPageNumber(currentPage) {
+    this.dispatchEvent(
+      new CustomEvent('page-selected', {detail: {currentPage}})
+    );
+    this.currentPage = currentPage;
+  }
+}
+
+window.customElements.define('listing-pagination', ListingPagination);
--- dev/index.js
+++ dev/index.js
@@ -1,3 +1,4 @@
 export {ListingUsers} from './views/listing-users';
 export {MyElement} from '../my-element';
 export {CustomButton} from './components/custom-button';
+export {ListingPagination} from './components/listing-pagination';
