4981fe4c28acd9381bf63266de0717a9ad482db0
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2024-11-07 13:46:19 +0300

    revert back my element

--- my-element.js
+++ my-element.js
@@ -68,50 +68,6 @@ export class MyElement extends LitElement {
    * @returns {string} A greeting directed at `name`
    */
   sayHello(name) {
-    const firstNames = [
-      'Ahmet',
-      'John',
-      'Jane',
-      'Emily',
-      'Michael',
-      'Sarah',
-      'David',
-      'Laura',
-      'Robert',
-      'Linda',
-    ];
-    const lastNames = [
-      'Sourtime',
-      'Doe',
-      'Smith',
-      'Johnson',
-      'Brown',
-      'Williams',
-      'Jones',
-      'Garcia',
-      'Miller',
-      'Davis',
-    ];
-
-    const mockUsers = [];
-
-    for (let i = 0; i < 100; i++) {
-      const firstName = firstNames[i % firstNames.length];
-      const lastName =
-        lastNames[Math.floor(i / firstNames.length) % lastNames.length];
-      mockUsers.push({
-        name: firstName,
-        lastName: lastName,
-        dateOfEmployment: '2022-01-01',
-        dateOfBirth: '1988-01-01',
-        phoneNumber: `+(90) 55555555${String(i).padStart(2, '0')}`,
-        email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}${i}@fake-ing.com`,
-        department: 'Analytics',
-        position: 'Junior',
-      });
-    }
-
-    console.log(mockUsers);
     return `Hello, ${name}`;
   }
 }
